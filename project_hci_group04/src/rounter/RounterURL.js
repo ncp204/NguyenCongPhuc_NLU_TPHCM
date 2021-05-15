d3a4c3cb2a3a0cedcdf059edf0e0d89f562e3e5f
luongluanmpt <luongluanmpt@gmail.com>
2021-05-15 23:13:21 +0700

    Luan xong VietNam

--- project_hci_group04/src/rounter/RounterURL.js
+++ project_hci_group04/src/rounter/RounterURL.js
@@ -3,7 +3,7 @@ import{BrowserRouter as Router, Route} from 'react-router-dom';
 import Download from '../component/Download';
 import Home from '../component/Home/Home';
 import Vaccin from '../component/Vaccin';
-import VietNam from '../component/VietNam';
+import VietNam from '../component/VietNam/VietNam';
 import World from '../component/World';
 
 class RounterURL extends Component {
